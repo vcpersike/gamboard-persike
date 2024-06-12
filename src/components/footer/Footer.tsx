38eb51a5a8c2cfc46e7701684272b19e95dc3d51
vcpersike <vcpersike@gmail.com>
2024-06-12 04:14:40 -0300

    teste 2

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+// src/app/components/Footer.tsx
+
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2024 My Next.js Site</p>
+
+    </footer>
+  );
+};
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+// src/app/components/Header.tsx
+
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>My Next.js Site</h1>
+
+    </header>
+  );
+};
+
